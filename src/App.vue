5e5dcb1f8cf08c0df0d61dd277e0056006eeae7d
salikovskiy <ssalikovskiy@gmail.com>
2021-11-13 00:08:32 +0200

    Added single card component and final touches

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,113 @@
 <template>
   <h1>Memory game</h1>
+  <button @click="shuffleCards">New Game</button>
+  <div class="card-grid">
+    <SingleCard v-for="(card, index) in cards" 
+      :key="index"
+      :card="card"
+      :match="card.matched"
+      @choice="handleChoice"
+      :flipped="card === choiceOne || card === choiceTwo || card.matched"
+    />
+  </div>
+  <h3>Turns: {{ turns }}</h3>
 </template>
 
 <script>
+import SingleCard from './components/SingleCard.vue'
+
+const cardImages = [
+  { src: 'https://i.ibb.co/QJTX1Wx/helmet-1.png', matched: false },
+  { src: 'https://i.ibb.co/4MBJSvn/potion-1.png', matched: false },
+  { src: 'https://i.ibb.co/4NPmCH2/ring-1.png', matched: false },
+  { src: 'https://i.ibb.co/rGmqRJX/scroll-1.png', matched: false },
+  { src: 'https://i.ibb.co/KXKJZLM/shield-1.png', matched: false },
+  { src: 'https://i.ibb.co/zncNpcf/sword-1.png', matched: false },
+]
 
 export default {
   name: 'App',
-  components: {}
+  data(){
+    return {
+      cards: [],
+      turns: 0,
+      choiceOne: null,
+      choiceTwo: null
+    }
+  },
+  methods: {
+    shuffleCards(){
+      this.cards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+
+      this.turns = 0
+    },
+    handleChoice(card){
+      this.choiceOne ? this.choiceTwo = card : this.choiceOne = card
+      if (this.choiceOne && this.choiceTwo){
+        if (this.choiceOne.src === this.choiceTwo.src) {
+          this.cards = this.cards.map((card) => {
+            if(card.src === this.choiceOne.src){
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+          console.log(this.cards);
+          this.resetTurn()
+        } else {
+          setTimeout(() => this.resetTurn(), 1000)
+        }
+      }
+    },
+    resetTurn() {
+      this.choiceOne = null
+      this.choiceTwo = null
+      this.turns = this.turns + 1
+    }
+  },
+  mounted(){
+    this.shuffleCards()
+  },
+  components: { SingleCard }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800&display=swap');
+
+/* base styles */
+body {
+  font-family: Raleway, sans-serif;
+  margin: 0;
+  font-size: 1.5em;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #1b1523;
+  color: #fff;
+}
+#app {
+  max-width: 860px;
+  margin: 40px auto;
+}
+button {
+  background: none;
+  border: 2px solid #fff;
+  padding: 6px 12px;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: bold;
+  cursor: pointer;
+  font-size: 1em;
+}
+button:hover {
+  background: #c23866;
+  color: #fff;
+}
+.card-grid {
+  margin-top: 40px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
 }
 </style>
---
+++ src/components/SingleCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="card" @click="handleClick">
+      <div :class="{'flipped': flipped}">
+        <img class="front" :src="card.src" alt="card front" />
+        <img class="back" src="https://i.ibb.co/YQyT987/cover.png"  alt="cover" />
+      </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ["card", 'matched', "flipped"],
+    methods: {
+        handleClick(){
+            this.$emit('choice', this.card)
+        }
+    }
+}
+</script>
+
+<style>
+.card {
+  position: relative;
+}
+.card img {
+  width: 100%;
+  display: block;
+  border: 2px solid #fff;
+  border-radius: 6px;
+}
+
+/* front of card - the picture */
+.card .front {
+  transform: rotateY(90deg);
+  transition: all ease-in 0.2s;
+  position: absolute;
+}
+.flipped .front {
+  transform: rotateY(0deg);
+  transition-delay: 0.2s;
+}
+
+/* back of card - cover */
+.card .back {
+  transition: all ease-in 0.2s;
+  transition-delay: 0.2s;
+}
+.flipped .back {
+  transform: rotateY(90deg);
+  transition-delay: 0s;
+}
+</style>
